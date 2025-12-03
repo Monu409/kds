247cd96e4472a60933d38a7b52e50dd30eea22ab
azusol-monu-macmini02 <monu.kumar@flashonmind.com>
2025-12-03 13:06:09 +0530

    add animation on item management 03-december

--- app/src/main/java/com/zotto/kds/restapi/ApiServices.kt
+++ app/src/main/java/com/zotto/kds/restapi/ApiServices.kt
@@ -91,3 +91,7 @@ interface ApiServices {
 }
 
 //https://api.opushospitalitymanager.com/api/getKdsOrders
+
+//https://api.opushospitalitymanager.com/api/getKdsOrders
+
+//https://api.opushospitalitymanager.com/api/getKdsOrders
